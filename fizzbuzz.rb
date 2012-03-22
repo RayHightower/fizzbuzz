3378414e15351fc69e8a375c1bcef04116a76b16
RayHightower <rth@wisdomgroup.com>
2012-03-22 03:09:19 -0500

    spacing

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,20 @@
 (1..100).each do |number|
+
   if (number % 3 == 0)
     print "Fizz"
     skipnum = true
   end
+
   if (number % 5 == 0)
     print "Buzz"
     skipnum = true
   end
+
   if (!skipnum)
     print number
   end
+
   print "\n"
+
 end
 
