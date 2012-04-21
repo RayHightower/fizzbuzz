22cb15cc49829667973efdf7cbbab5073ff46a11
RayHightower <rth@wisdomgroup.com>
2012-04-21 18:52:51 -0500

    lots & lots of failing tests!

--- kata01/lib/fizzbuzz.rb
+++ kata01/lib/fizzbuzz.rb
@@ -2,7 +2,16 @@ class FizzBuzz
 
   def self.calc(this_number)
 
-    return 1
+    if this_number % 3 == 0
+      return "fizz"
+    end
+
+    if this_number % 5 == 0
+      return "buzz"
+    end
     
+    return this_number
+    
+
   end
 end
--- kata01/spec/fizzbuzz_spec.rb
+++ kata01/spec/fizzbuzz_spec.rb
@@ -1,7 +1,21 @@
 require_relative '../lib/fizzbuzz'
 
 describe "return something" do
-  it "should return something" do
-    FizzBuzz.calc(1).should == 1
+
+    this_number = 1 
+    while this_number <= 100 do
+      print this_number.to_s + "\n"
+      it "should return fizz for multiples of 3" do
+          FizzBuzz.calc(this_number).should == "fizz" if this_number % 3 == 0
+          print FizzBuzz.calc.to_s + "\n"
+      end
+      it "should return the number that we give it" do
+        FizzBuzz.calc(this_number).should == this_number
+      end
+      this_number += 1
+    end
+
+  it "should return buzz for multiples of 5" do
+    FizzBuzz.calc(5).should == "buzz"
   end
 end
