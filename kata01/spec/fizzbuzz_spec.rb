4705b515162be9a65ebcbe18da82ea4a1ea75c54
RayHightower <rth@wisdomgroup.com>
2012-04-21 18:57:20 -0500

    tests are passing, but I think they are wrong

--- kata01/spec/fizzbuzz_spec.rb
+++ kata01/spec/fizzbuzz_spec.rb
@@ -4,18 +4,14 @@ describe "return something" do
 
     this_number = 1 
     while this_number <= 100 do
-      print this_number.to_s + "\n"
       it "should return fizz for multiples of 3" do
           FizzBuzz.calc(this_number).should == "fizz" if this_number % 3 == 0
-          print FizzBuzz.calc.to_s + "\n"
+          print FizzBuzz.calc(this_number).to_s + "\n"
       end
       it "should return the number that we give it" do
         FizzBuzz.calc(this_number).should == this_number
+        print this_number.to_s + "\n"
       end
       this_number += 1
     end
-
-  it "should return buzz for multiples of 5" do
-    FizzBuzz.calc(5).should == "buzz"
-  end
 end
