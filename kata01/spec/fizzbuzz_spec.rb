189a0fbf2366602065a25bc616abf51bf367c24b
RayHightower <rth@wisdomgroup.com>
2012-04-22 08:54:18 -0500

    appears to be working, but tests are failing. Why?

--- kata01/spec/fizzbuzz_spec.rb
+++ kata01/spec/fizzbuzz_spec.rb
@@ -5,7 +5,13 @@ describe "return something" do
     this_number = 1 
     while this_number <= 100 do
 
-      if this_number % 3 == 0
+      if this_number % 15 == 0
+        it "should return fizzbizz for multiples of 15" do
+          FizzBuzz.calc(this_number).should == "fizzbuzz"
+        end
+        print this_number.to_s + " " + FizzBuzz.calc(this_number).to_s + " mults of 15\n"
+
+      elsif this_number % 3 == 0
         it "should return fizz for multiples of 3" do
           FizzBuzz.calc(this_number).should == "fizz"
         end
