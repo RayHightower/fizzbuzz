7b736fd6d4de9c4f39a6b27470d8794c49ea7753
RayHightower <rth@wisdomgroup.com>
2012-04-22 18:35:19 -0500

    step by step, up to 10

--- kata02/lib/fizzbuzz.rb
+++ kata02/lib/fizzbuzz.rb
@@ -2,6 +2,10 @@ class FizzBuzz
 
   def self.calc(this_number)
 
+    return "fizz" if this_number % 3 == 0
+
+    return "buzz" if this_number % 5 == 0
+
     return this_number
     
   end
--- kata02/spec/fizzbuzz_spec.rb
+++ kata02/spec/fizzbuzz_spec.rb
@@ -10,4 +10,36 @@ describe "one step at a time" do
     FizzBuzz.calc(2).should == 2
   end
 
+  it "should return fizz when given 3" do
+    FizzBuzz.calc(3).should == "fizz"
+  end
+
+  it "should return 4" do
+    FizzBuzz.calc(4).should == 4
+  end
+
+  it "should return buzz when given 5" do
+    FizzBuzz.calc(5).should == "buzz"
+  end
+
+  it "should return fizz when given 6" do
+    FizzBuzz.calc(6).should == "fizz"
+  end
+
+  it "should return 7" do
+    FizzBuzz.calc(7).should == 7
+  end
+
+  it "should return 8" do
+    FizzBuzz.calc(8).should == 8
+  end
+
+  it "should return fizz when given 9" do
+    FizzBuzz.calc(9).should == "fizz"
+  end
+
+  it "should return buzz when given 10" do
+    FizzBuzz.calc(10).should == "buzz"
+  end
+
 end
