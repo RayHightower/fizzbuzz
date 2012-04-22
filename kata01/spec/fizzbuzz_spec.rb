e25f438fe51a47e196ba75d707e98486bf923f10
RayHightower <rth@wisdomgroup.com>
2012-04-21 19:18:28 -0500

    clearly, this is not ping pong

--- kata01/spec/fizzbuzz_spec.rb
+++ kata01/spec/fizzbuzz_spec.rb
@@ -4,14 +4,25 @@ describe "return something" do
 
     this_number = 1 
     while this_number <= 100 do
-      it "should return fizz for multiples of 3" do
-          FizzBuzz.calc(this_number).should == "fizz" if this_number % 3 == 0
+      if this_number % 3 == 0
+        it "should return fizz for multiples of 3" do
+          FizzBuzz.calc(this_number).should == "fizz"
           print FizzBuzz.calc(this_number).to_s + "\n"
+        end
+
+      elseif this_number % 5 == 0
+        it "should return buzz for multiples of 5" do
+          FizzBuzz.calc(this_number).should == this_number
+          print this_number.to_s + "\n"
+        end
+      else
+        it "should return the number that we give it" do
+          FizzBuzz.calc(this_number).should == this_number
+          print this_number.to_s + "\n"
+        end
       end
-      it "should return the number that we give it" do
-        FizzBuzz.calc(this_number).should == this_number
-        print this_number.to_s + "\n"
-      end
+
+
       this_number += 1
     end
 end
