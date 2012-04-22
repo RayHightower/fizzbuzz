20923f70150590bbfae3e1da0d9cc97d2e8c97e5
RayHightower <rth@wisdomgroup.com>
2012-04-22 08:38:24 -0500

    print statements work better outside of the specs

--- kata01/spec/fizzbuzz_spec.rb
+++ kata01/spec/fizzbuzz_spec.rb
@@ -4,25 +4,24 @@ describe "return something" do
 
     this_number = 1 
     while this_number <= 100 do
-      print this_number.to_s + " just the number this time.\n"
 
       if this_number % 3 == 0
         it "should return fizz for multiples of 3" do
           FizzBuzz.calc(this_number).should == "fizz"
-          print this_number.to_s + " " + FizzBuzz.calc(this_number).to_s + " mults of 3\n"
         end
+        print this_number.to_s + " " + FizzBuzz.calc(this_number).to_s + " mults of 3\n"
 
       elsif this_number % 5 == 0
         it "should return buzz for multiples of 5" do
           FizzBuzz.calc(this_number).should == this_number
-          print this_number.to_s + " " + FizzBuzz.calc(this_number).to_s + " mults of 5\n"
         end
+        print this_number.to_s + " " + FizzBuzz.calc(this_number).to_s + " mults of 5\n"
 
       else
         it "should return the number that we give it" do
           FizzBuzz.calc(this_number).should == this_number
-          print this_number.to_s + "\n"
         end
+        print this_number.to_s + "\n"
       end
 
 
