8f17b65e37f8a0af73739a249eee1eaff022a8fd
RayHightower <rth@wisdomgroup.com>
2012-04-22 23:14:16 -0500

    cleaned up kata01

--- kata01/spec/fizzbuzz_spec.rb
+++ kata01/spec/fizzbuzz_spec.rb
@@ -7,31 +7,26 @@ describe "fizz or buzz: " do
     counters.each do |counter|
 
       result = FizzBuzz.calc(counter)
-      print "\nresult = " + result.to_s + " counter = " + counter.to_s + "\n"
           
       if counter % 15 == 0
         it "should return fizzbizz for multiples of 15" do
           result.should == "fizzbuzz"
         end
-        print counter.to_s + " " + result + " mults of 15 with refactoring.\n"
 
       elsif counter % 3 == 0
         it "should return fizz for multiples of 3" do
           result.should == "fizz"
         end
-        print counter.to_s + " " + result + " mults of 3\n"
 
       elsif counter % 5 == 0
         it "should return buzz for multiples of 5" do
           result.should == "buzz"
         end
-        print counter.to_s + " " + result + " mults of 5\n"
 
       else
         it "should return the number that we give it" do
           result.should == counter.to_s
         end
-        print result + " is the number that we gave it.\n"
       end
 
     end
