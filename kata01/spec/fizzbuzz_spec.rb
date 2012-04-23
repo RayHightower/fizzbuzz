bffe8c1d67a42f1dfa0195f27fe2fd2980530d69
RayHightower <rth@wisdomgroup.com>
2012-04-22 23:11:14 -0500

    using an array was a better move. kata01 complete.

--- kata01/spec/fizzbuzz_spec.rb
+++ kata01/spec/fizzbuzz_spec.rb
@@ -2,38 +2,37 @@ require_relative '../lib/fizzbuzz'
 
 describe "fizz or buzz: " do
 
-    this_number = 1 
+    counters = (1..100).to_a 
 
-    while this_number < 101 do
+    counters.each do |counter|
 
-      result = FizzBuzz.calc(this_number)
-      print "\nresult = " + result.to_s + " this_number = " + this_number.to_s + "\n"
+      result = FizzBuzz.calc(counter)
+      print "\nresult = " + result.to_s + " counter = " + counter.to_s + "\n"
           
-      if this_number % 15 == 0
+      if counter % 15 == 0
         it "should return fizzbizz for multiples of 15" do
           result.should == "fizzbuzz"
         end
-        print this_number.to_s + " " + result + " mults of 15 with refactoring.\n"
+        print counter.to_s + " " + result + " mults of 15 with refactoring.\n"
 
-      elsif this_number % 3 == 0
+      elsif counter % 3 == 0
         it "should return fizz for multiples of 3" do
           result.should == "fizz"
         end
-        print this_number.to_s + " " + result + " mults of 3\n"
+        print counter.to_s + " " + result + " mults of 3\n"
 
-      elsif this_number % 5 == 0
+      elsif counter % 5 == 0
         it "should return buzz for multiples of 5" do
           result.should == "buzz"
         end
-        print this_number.to_s + " " + result + " mults of 5\n"
+        print counter.to_s + " " + result + " mults of 5\n"
 
       else
         it "should return the number that we give it" do
-          result.should == this_number.to_s
+          result.should == counter.to_s
         end
         print result + " is the number that we gave it.\n"
       end
 
-      this_number += 1
     end
 end
