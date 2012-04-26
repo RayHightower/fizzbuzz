3f9f96296c444de1cc5575c0953ea05e8da4655b
RayHightower <rth@wisdomgroup.com>
2012-04-26 18:34:10 -0500

    kata complete

--- kata03/lib/fizzbuzz.rb
+++ kata03/lib/fizzbuzz.rb
@@ -1,6 +1,9 @@
 class FizzBuzz
 
   def self.calc(this_number)
-    1
+    return "fizzbuzz" if this_number % 15 == 0
+    return "buzz" if this_number % 5 == 0
+    return "fizz" if this_number % 3 == 0
+    this_number
   end
 end
--- kata03/spec/fizzbuzz_spec.rb
+++ kata03/spec/fizzbuzz_spec.rb
@@ -1,7 +1,25 @@
 require_relative '../lib/fizzbuzz'
 
-describe "return something. anything." do
-  it 'should return something' do
-    FizzBuzz.calc(1).should == 1
+describe "return fizz, buzz, or the integer" do
+  
+  (1..100).each do |this_number|
+
+    if this_number % 15 == 0
+      it 'should return buzz for multiples of 5' do
+        FizzBuzz.calc(15).should == "fizzbuzz" 
+      end
+    elsif this_number % 5 == 0
+      it 'should return buzz for multiples of 5' do
+        FizzBuzz.calc(5).should == "buzz" 
+      end
+    elsif this_number % 3 == 0
+      it 'should return fizz for multiples of 3' do
+        FizzBuzz.calc(3).should == "fizz" 
+      end
+    else
+      it 'should return the integer we give it if not FB' do
+        FizzBuzz.calc(this_number).should == this_number
+      end
+    end
   end
 end
