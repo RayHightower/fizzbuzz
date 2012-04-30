1184544d0a0db9f7d067d6d51c2ef26c358d7d72
RayHightower <rth@wisdomgroup.com>
2012-04-29 21:20:15 -0500

    all conditions met. now doing stress tests.

--- kata04/spec/fizzbuzz_spec.rb
+++ kata04/spec/fizzbuzz_spec.rb
@@ -1,7 +1,30 @@
 require_relative '../lib/fizzbuzz'
 
 describe 'return the integer we give to it' do
+  this_number = 19
+
   it 'shoud return the integer we give to it' do
-    FizzBuzz.calc(1).should == 1
+    FizzBuzz.calc(this_number).should == this_number
+  end
+  it 'shoud return fizz for 3' do
+    FizzBuzz.calc(3).should == 'fizz'
+  end
+  it 'shoud return buzz for 5' do
+    FizzBuzz.calc(5).should == 'buzz'
+  end
+  it 'shoud return fizzbuzz for 15' do
+    FizzBuzz.calc(15).should == 'fizzbuzz'
+  end
+end
+
+describe 'return fizz, buzz, fizzbuzz, or the integer as appropriate' do
+
+  (1..1000).each do |this_number|
+    if this_number % 15 == 0
+      it 'shoud return fizzbuzz' do
+        FizzBuzz.calc(this_number).should == 'fizzbuzz'
+      end
+    end
+
   end
 end
