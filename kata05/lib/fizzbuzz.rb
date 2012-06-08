ed0dd70d1e463b127f46f739850251c02b152d76
RayHightower <rth@wisdomgroup.com>
2012-06-07 22:45:41 -0500

    kata complete

--- kata05/lib/fizzbuzz.rb
+++ kata05/lib/fizzbuzz.rb
@@ -1,9 +1,9 @@
 class FizzBuzz
 
   def self.calc(this_number)
-    return 'fizz' if this_number == 3
-    return 'buzz' if this_number == 5
-    return 'fizzbuzz' if this_number == 15
+    return 'fizzbuzz' if this_number % 15 == 0
+    return 'fizz' if this_number % 3 == 0
+    return 'buzz' if this_number % 5 == 0
     return this_number
   end
 end
--- kata05/spec/fizzbuzz_spec.rb
+++ kata05/spec/fizzbuzz_spec.rb
@@ -15,6 +15,26 @@ describe 'return the number we give it' do
   it 'should return fizzbuzz for 15' do
     FizzBuzz.calc(15).should == 'fizzbuzz'
   end
+
+  (1..100).each do |another_number|
+    if another_number % 15 == 0
+      it 'should return buzz for multiples of 15' do
+         FizzBuzz.calc(another_number).should == 'fizzbuzz'
+      end
+    elsif another_number % 3 == 0
+      it 'should return fizz for multiples of 3' do
+         FizzBuzz.calc(another_number).should == 'fizz'
+      end
+    elsif another_number % 5 == 0
+      it 'should return buzz for multiples of 5' do
+         FizzBuzz.calc(another_number).should == 'buzz'
+      end
+    else
+      it 'should return the original number' do
+        FizzBuzz.calc(another_number).should == another_number
+      end
+    end
+  end
 end
 
 
