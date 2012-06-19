bd932e70b3d11d87f78251d71a3cd3aa7c147062
RayHightower <rth@wisdomgroup.com>
2012-06-19 16:23:00 -0500

    kata complete

--- kata06/lib/fizzbuzz.rb
+++ kata06/lib/fizzbuzz.rb
@@ -1,5 +1,8 @@
 class FizzBuzz
   def self.calc(that_number)
+    return 'fizzbuzz' if that_number % 15 == 0
+    return 'buzz' if that_number % 5 == 0
+    return 'fizz' if that_number % 3 == 0
     that_number
     
   end
--- kata06/spec/fizzbuzz_spec.rb
+++ kata06/spec/fizzbuzz_spec.rb
@@ -1,10 +1,38 @@
 require_relative '../lib/fizzbuzz.rb'
 
-describe 'return the integer we give it' do
+describe 'return simple fizz & buzz operations' do
   this_number = 92
 
   it 'should return the number if not fizz or buzz' do
     FizzBuzz.calc(this_number).should == this_number
   end
 
+  it 'should return fizz if the number is 3' do
+    FizzBuzz.calc(3).should == 'fizz'
+  end
+
+  it 'should return buzz if the number is 5' do
+    FizzBuzz.calc(5).should == 'buzz'
+  end
+end
+describe 'return simple fizz & buzz operations' do
+  (1..100).each do |this_number|
+    if this_number % 15 == 0
+      it 'should return fizzbuzz for multiples of 15' do
+        FizzBuzz.calc(this_number).should == 'fizzbuzz'
+      end
+    elsif this_number % 5 == 0
+      it 'should return buzz for multiples of 5' do
+        FizzBuzz.calc(this_number).should == 'buzz'
+      end
+    elsif this_number % 3 == 0
+      it 'should return fizz for multiples of 3' do
+        FizzBuzz.calc(this_number).should == 'fizz'
+      end
+    else
+      it 'should return the original integer' do
+        FizzBuzz.calc(this_number).should == this_number
+      end
+    end
+  end
 end
